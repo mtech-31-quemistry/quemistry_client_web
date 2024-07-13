408fdf5f1e25babf7ef10d51164f9df26ca6530f
Raymond Htet <htetthurazaw@gmail.com>
2024-07-13 13:35:17 +0800

    add static route

--- next.config.js
+++ next.config.js
@@ -3,7 +3,17 @@ const nextConfig = {
     output: 'export',
     distDir: 'dist',
     reactStrictMode: false,
-    trailingSlash: true
+    trailingSlash: true,
+    exportPathMap: async function (
+        defaultPathMap,
+        { dev, dir, outDir, distDir, buildId }
+      ) {
+        return {
+          '/': { page: '/' },
+          '/dashboard': { page: '/dashboard/' },
+        }
+      },
+    
 }
 
 module.exports = nextConfig;
