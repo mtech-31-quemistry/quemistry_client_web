662b9112593d662976d9cb96971801b46bbff5d2
Wang Shiji <scglwsj@163.com>
2024-08-03 23:53:49 +0800

    add class button

--- app/(main)/classes/page.tsx
+++ app/(main)/classes/page.tsx
@@ -1,14 +1,28 @@
 /* eslint-disable @next/next/no-img-element */
 'use client';
 import { Button } from 'primereact/button';
-import React, { useContext, useEffect, useRef, useState } from 'react';
-import { LayoutContext } from '../../../layout/context/layoutcontext';
-import { ChartData, ChartOptions } from 'chart.js';
+import React, { Fragment, useContext, useEffect, useRef, useState } from 'react';
+import { ChartOptions } from 'chart.js';
+import { Dialog } from 'primereact/dialog';
+import { InputText } from 'primereact/inputtext';
+import { LayoutContext } from '@/layout/context/layoutcontext';
 
 const Classes = () => {
+    const [addClass, setAddClass] = useState(false);
+    const [newClassCode, setNewClassCode] = useState("");
+    const [newClassDescription, setNewClassDescription] = useState("");
+    const [newClassEducationLevel, setNewClassEducationLevel] = useState("");
+    const [newClassSubject, setNewClassSubject] = useState("");
 
     const { layoutConfig } = useContext(LayoutContext);
 
+    const clearNewClass = () => {
+        setAddClass(false)
+        setNewClassCode("")
+        setNewClassDescription("")
+        setNewClassEducationLevel("")
+        setNewClassSubject("")
+    }
     const applyLightTheme = () => {
         const lineOptions: ChartOptions = {
             plugins: {
@@ -38,7 +52,6 @@ const Classes = () => {
             }
         };
 
-        //setLineOptions(lineOptions);
     };
 
     const applyDarkTheme = () => {
@@ -69,13 +82,37 @@ const Classes = () => {
                 }
             }
         };
-
-        //setLineOptions(lineOptions);
     };
 
-    // useEffect(() => {
-    //     ProductService.getProductsSmall().then((data) => setProducts(data));
-    // }, []);
+    const saveClass = () => {
+        clearNewClass()
+        const newClass:Class = {
+            code: newClassCode,
+            description: newClassDescription,
+            educationLevel: newClassEducationLevel,
+            subject: newClassSubject
+        }
+
+        console.log(newClass)
+    }
+
+    const addClassFooter = (
+        <div>
+            <Button label="Cancel" icon="pi pi-times" onClick={(()=>clearNewClass())} className="p-button-text" />
+            <Button label="Save" icon="pi pi-save" onClick={() => saveClass()} autoFocus />
+        </div>
+    )
+
+    const renderField = (labelTextName: string,  value: string, onChange: (e: React.ChangeEvent<HTMLInputElement>) => void) => (
+        <div className="field grid">
+            <label htmlFor={labelTextName} className="col-12 mb-2 md:col-2 md:mb-0">
+                {labelTextName}
+            </label>
+            <div className="col-12 md:col-10">
+                <InputText placeholder={labelTextName} style={{ width: '30vw' }} value={value} onChange={onChange} />
+            </div>
+        </div>
+    );
 
     useEffect(() => {
         if (layoutConfig.colorScheme === 'light') {
@@ -89,11 +126,16 @@ const Classes = () => {
         <div className="grid">
             <div className="col-12 xl:col-6">
                 <div className="card">
-                    <Button label="New" icon="pi pi-plus" onClick={() => {
-                        // API Call
-                    }}
-                    />
-                    <h5>Classes</h5>
+                    <Fragment>
+                        <Button label="New" icon="pi pi-plus" onClick={(e) => setAddClass(true)}  />
+                    </Fragment>
+                    <Dialog header="Add Class"  style={{ width: '50vw' }} visible={addClass} onHide={() => {addClass && clearNewClass()}} footer={addClassFooter}>
+                        {renderField("Class Code",  newClassCode, (e) => setNewClassCode(e.target.value))}
+                        {renderField("Class Description",  newClassDescription, (e) => setNewClassDescription(e.target.value))}
+                        {renderField("Class Education Level",  newClassEducationLevel,  (e) => setNewClassEducationLevel(e.target.value))}
+                        {renderField("Class Subject",   newClassSubject, (e) => setNewClassSubject(e.target.value))}
+                    </Dialog>
+                    <h5>Manage Classes</h5>
                 </div>
             </div>
         </div>
--- types/user.ts
+++ types/user.ts
@@ -1,3 +1,10 @@
 interface UserProfile {
     email: string;
 }
+
+interface Class{
+    code: string;
+    description: string;
+    educationLevel: string;
+    subject: string;
+}
