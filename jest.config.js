bab964f68041cb190ba1c3efbf5b9f04c638fd70
andrewtan2000 <andrew_tan2000@hotmail.com>
2024-08-07 23:28:29 +0800

    upload coverage reports to sonarcloud

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,9 @@
 module.exports = {
     testEnvironment: 'jest-environment-jsdom',
+    collectCoverage: true,
+    collectCoverageFrom: [
+      'src/**/*.{js,jsx,ts,tsx}',
+      '!src/**/*.d.ts',
+    ],
+    coverageDirectory: 'coverage',
   };
