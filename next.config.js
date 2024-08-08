dc13953d37ec695d3150a118082601dfc1fdbba9
linxiaoxin <lin_xiaoxin@hotmail.com>
2024-08-08 15:19:27 +0800

    Resolve build issues with babel

--- next.config.js
+++ next.config.js
@@ -6,18 +6,20 @@ const nextConfig = {
     trailingSlash: false
 }
 
-module.exports = {
-    nextConfig,
-    reactStrictMode: true,
-    swcMinify: true,
-    // If you have custom Babel configuration, ensure it includes the automatic runtime
-    babel: {
-        presets: [
-        ["next/babel", {
-            "preset-react": {
-            "runtime": "automatic"
-            }
-        }]
-        ]
-    }
-}
+module.exports = nextConfig
+
+// module.exports = {
+//     nextConfig,
+//     reactStrictMode: true,
+//     swcMinify: true,
+//     // If you have custom Babel configuration, ensure it includes the automatic runtime
+//     babel: {
+//         presets: [
+//         ["next/babel", {
+//             "preset-react": {
+//             "runtime": "automatic"
+//             }
+//         }]
+//         ]
+//     }
+// }
