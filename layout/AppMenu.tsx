4bdca18e0a440b1d06d9ba58d98ad5213bd59d90
andrewtan2000 <andrew_tan2000@hotmail.com>
2024-09-17 01:01:44 +0800

    Update AppMenu.tsx

--- layout/AppMenu.tsx
+++ layout/AppMenu.tsx
@@ -34,14 +34,21 @@ const AppMenu = () => {
         {
             label: 'Home',
             items: [
-                { label: 'Dashboard', icon: 'pi pi-fw pi-home', to: '/dashboard' },
-                { label: 'Take Quiz', icon: 'pi pi-fw pi-pencil', to: '/quiz', visible: accessibleBy(['student']) },
-                { label: 'Class Test', icon: 'pi pi-fw pi-graduation-cap', to: '/test', visible: accessibleBy(['student']) },
                 {
-                    label: 'History',
-                    icon: 'pi pi-fw pi-history',
-                    to: '/quiz/history',
-                    visible: accessibleBy(['student'])
+                    label: 'Dashboard',
+                    icon: 'pi pi-fw pi-home',
+                    to: '/dashboard',
+                    items: [
+                        { label: 'Take Quiz', icon: 'pi pi-fw pi-pencil', to: '/quiz', visible: accessibleBy(['student']) },
+                        { label: 'Quiz Results', icon: 'pi pi-fw pi-chart-scatter', to: '/quiz/results', visible: accessibleBy(['student']) },
+                        { label: 'Class Test', icon: 'pi pi-fw pi-graduation-cap', to: '/test', visible: accessibleBy(['student']) },
+                        {
+                            label: 'History',
+                            icon: 'pi pi-fw pi-history',
+                            to: '/quiz/history',
+                            visible: accessibleBy(['student'])
+                        }
+                    ]
                 }
             ]
         },
