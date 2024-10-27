ac6f77aa1460714ac4376b4d7ff187cdae1448db
ernestjx <73307945+ernestjx@users.noreply.github.com>
2024-10-27 19:46:30 +0800

    test coverage config

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -17,7 +17,7 @@ export default defineConfig({
             reporter: ['text', 'json', 'html', 'lcov'],
             reportsDirectory: './coverage',
             exclude: [
-                'service/**', // Exclude specific folders or files
+                // 'service/**', // Exclude specific folders or files
                 'layout/**', // Exclude specific folders or files
                 '**/*.mjs',
             ]
